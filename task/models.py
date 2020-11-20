390715e50d0dfefed28f1667e15f2cc5239f7111
Dipesh Sharma <60509313+DipeshSharma25@users.noreply.github.com>
2020-11-21 03:34:42 +0530

    Final tested

--- task/models.py
+++ task/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#email id unique
+User._meta.get_field('email')._unique = True
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
+from django.http import HttpResponse, HttpResponseRedirect
 from rest_framework.views import APIView
 from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
@@ -34,14 +35,17 @@ class RegisterView(APIView):
         address = request.POST.get('address')
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
-
+        print(latitude,longitude)
         # using " ".join() in case user's name contains more than 2 words
-        User.objects.get_or_create(first_name=name[0],last_name=" ".join(name[1:]),email=email,username=username,password=make_password(password))
+        # User.objects.get_or_create(first_name=name[0],last_name=" ".join(name[1:]),email=email,username=username,password=make_password(password))
 
-        Profile.objects.create(user=User.objects.get(username=username),mobile=mobile,address=address,latitude=latitude,longitude=longitude)
-        user = authenticate(username=User.objects.get(email=email),password=password)
-        return redirect('task')
-        return Response({'message': 'success'},status=HTTP_200_OK)
+        # Profile.objects.create(user=User.objects.get(username=username),mobile=mobile,address=address,latitude=latitude,longitude=longitude)
+        # try:
+        #     user = authenticate(username=User.objects.get(username=username ),password=password)
+        # except Exception as e:
+        #     print(e)
+        # login(request, user)
+        return HttpResponseRedirect(redirect_to='/task/')
 
 class LoginView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
@@ -59,7 +63,7 @@ class LoginView(APIView):
         except:
             return Response({"message": "invalid"},status=HTTP_400_BAD_REQUEST)
         login(request, user)
-        return redirect('task')
+        return HttpResponseRedirect(redirect_to='/task/')
 
         # return Response({'message': 'success'},status=HTTP_200_OK)
 
@@ -80,6 +84,6 @@ class TaskView(APIView):
         assigned_to = User.objects.get(id=request.POST.get('assigned_to')) 
         date_time = datetime.strptime( f"{request.POST.get('date')} {request.POST.get('time')}", '%Y-%m-%d %H:%M')
         Task.objects.create(task_name=name,assigned_to=assigned_to,assigned_by=request.user,date_time=date_time)
-        return redirect('task')
+        return HttpResponseRedirect(redirect_to='/task/')
         
         # return Response({'message': 'success'},status=HTTP_200_OK)
